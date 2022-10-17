daee9e2fd5034fe81cab1a060eb9f8bcdb5403b8
천수민 <102133017+smthousand@users.noreply.github.com>
2022-10-17 16:23:29 +0900

    updated

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const tabs = document.querySelectorAll('.tab')
+const panels = document.querySelectorAll('.panel')
+
+// Tabs menu event listener
+tabs.forEach((tab) => tab.addEventListener('click', onTabClick))
+
+function onTabClick(e) {
+    // Deactivate all tabs
+    tabs.forEach(tab => {
+        tab.children[0].classList.remove(
+            'border-btBk', 
+            'border-b-2'
+        )
+    })
+
+    // Hide all panels
+    panels.forEach((panel) => panel.classList.add('hidden'))
+
+    // Activate a new tab and panel based on th target
+    e.target.classList.add('border-btBk', 'border-b-2')
+    const classString = e.target.getAttribute('data-target')
+    document.getElementById('panels')
+    .getElementsByClassName(classString)[0]
+    .classList.remove('hidden')
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ['./*.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        btYellow: 'hsl(57, 97%, 71%)',
+        btBk20: 'hsl(240, 20%, 98%)',
+        btBk40: 'hsl(240, 9%, 89%)',
+        btBk60: 'hsl(240, 3%, 74%)',
+        btBk80: 'hsl(240, 1%, 50%)',
+        btBk: 'hsl(240, 2%, 25%)',
+      },
+      fontFamily: {
+        sans: ['SUIT Variable', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
